deacaf549cde4fca23935db9d89afec93818e1b1
greatgrz <ruizhen.guo@gmail.com>
2017-04-27 18:58:28 -0700

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -3,3 +3,21 @@
 //IMPORTANT!
 unsigned long tn = long(n);
 tn = labs(tn);
+
+
+//customize comapre function in sort 
+class Solution {
+public:
+    static bool compare_fun(Interval& a, Interval& b){
+        return a.start < b.start; 
+    }
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if(intervals.size() == 0) return true;
+        sort(intervals.begin(), intervals.end(), compare_fun);
+        for(int i = 0; i < intervals.size()-1; i++){
+            if (intervals[i].end > intervals[i+1].start)
+                return false;
+        }
+        return true;
+    }
+};
